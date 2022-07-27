89b5b814339ede4c164c1d0673cba7b2af148524
pocha <arinze6@gmail.com>
2022-07-27 16:39:22 -0700

    initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 's4cvlsu1j4';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-g9ogxn0g.us.auth0.com',
+  clientId: 'TSQfVhTUQZL3Y0hcvKRtrEgqDJ3eSETg',
+  callbackUrl: 'http://localhost:3000/callback'
+};
